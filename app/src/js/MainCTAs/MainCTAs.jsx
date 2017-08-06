6211c5bea4b1b4d7ae4224b1d9bd14aede1940a0
Tom  Christian <tctc91@gmail.com>
2017-08-06 11:27:23 +0100

    cta hovers

--- app/src/js/MainCTAs/MainCTAs.jsx
+++ app/src/js/MainCTAs/MainCTAs.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { scrollToElement } from '../helpers';
 
 const cvDownloadUrl = 'https://www.dropbox.com/s/4m28soy02jkc4zf/tc2017.pdf?dl=0';
-const btnClass = 'btn btn-outline-secondary btn-lg';
+const btnClass = (type = 'secondary') => `btn btn-outline-${type} btn-lg`;
 
 const MainCTAs = () => {
   return (
     <div className="main-ctas">
-      <a className={`main-ctas__btn main-ctas__btn--folio ${btnClass}`} href="#tc-projects" onClick={e => scrollToElement(e, 'tc-projects')}>View projects</a>
+      <a className={`main-ctas__btn main-ctas__btn--folio ${btnClass('success')}`} href="#tc-projects" onClick={e => scrollToElement(e, 'tc-projects')}>View projects</a>
       <span className="main-ctas__sep hidden-xs-down">or</span>
-      <a className={`main-ctas__btn main-ctas__btn--cv ${btnClass}`} href={cvDownloadUrl} target="_blank">Download CV</a>
+      <a className={`main-ctas__btn main-ctas__btn--cv ${btnClass()}`} href={cvDownloadUrl} target="_blank">Download CV</a>
     </div>
   );
 };
