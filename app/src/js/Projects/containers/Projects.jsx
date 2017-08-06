e34bc6173bdad71ae0e1d38382fdc7231605f5e9
Tom  Christian <tctc91@gmail.com>
2017-08-06 15:11:14 +0100

    organised timeline stuff

--- app/src/js/Projects/containers/Projects.jsx
+++ app/src/js/Projects/containers/Projects.jsx
@@ -5,32 +5,9 @@ import { scrollToElement, extractErrors } from '../../helpers';
 
 import { Errors } from '../../Errors/components';
 import { ProjectListing } from '../components';
+import { TimelineStem, TimelineCircle } from '../../Timeline';
 
 import getProjectYear from '../helpers';
-
-const TimelineCircle = ({ size, half }) => {
-  return (
-    <svg width={size / 2} height={size} xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 84.1 168.2`}>
-      <g>
-        {half === 'left' ?
-          <path className={`circle circle--${half}`} d="M84.1 1A83.1 83.1 0 0 0 1 84.1a83.1 83.1 0 0 0 83.1 83.1" />
-          :
-          <path className={`circle circle--${half}`} d="M0,1H0A83.1,83.1,0,0,1,83.1,84.1h0A83.1,83.1,0,0,1,0,167.2H0" />
-        }
-      </g>
-    </svg>
-  );
-};
-
-const TimelineStem = () => {
-  return (
-    <svg width="2" height="35" className="timeline__stem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2 36.62">
-      <g>
-        <path className="timeline__stem-path" d="M1 0v36.62" />
-      </g>
-    </svg>
-  );
-};
 
 class Projects extends React.Component {
   constructor(props) {
@@ -56,16 +33,15 @@ class Projects extends React.Component {
 
     return (
       <div className="projects">
-
         {currentYear ?
           <a
             onClick={e => scrollToElement(e, 'tc-projects')}
             href="#tc-projects"
-            className="timeline-marker timeline-marker--current"
+            className="timeline-cta"
           >
             <TimelineCircle size={93} half="left" />
             <TimelineCircle size={93} half="right" />
-            <span className="timeline-marker__label display-4">{currentYear}</span>
+            <span className="timeline-cta__label display-4">{currentYear}</span>
             <TimelineStem />
           </a>
           : null
---
+++ app/src/js/Timeline/TimelineStem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TimelineStem = () => {
+  return (
+    <svg width="2" height="35" className="timeline-stem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2 36.62">
+      <g>
+        <path className="timeline-stem__path" d="M1 0v36.62" />
+      </g>
+    </svg>
+  );
+};
+
+export default TimelineStem;
