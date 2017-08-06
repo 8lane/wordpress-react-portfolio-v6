b883a7d4db134c2883c54e3a83630e17911c97ad
Tom  Christian <tctc91@gmail.com>
2017-08-06 20:29:51 +0100

    footer styled

--- footer.php
+++ footer.php
@@ -15,24 +15,43 @@
 
 	<footer class="footer text-center">
 		<div class="container">
-			<?php dynamic_sidebar( 'footer-1' ); ?>
+
+			<div class="footer__contact text-center">
+				<i class=""></i> Interested in working with me?
+				<a class="btn btn-sm btn-primary footer__link footer__link--contact" href="mailto:hello@elseif.io">Let's chat</a>
+			</div>
 
 			<ul class="footer__social list-unstyled">
 				<li class="footer__item footer__item--social list-inline-item">
-					<a class="footer__link" href="https://uk.linkedin.com/in/tomchristian1">LinkedIn</a>
+					<a class="footer__icon footer__icon--linkedin" href="https://uk.linkedin.com/in/tomchristian1">
+						<i class="icon-linkedin"></i>
+						<span class="sr-only">LinkedIn</span>
+					</a>
 				</li>
 				<li class="footer__item footer__item--social list-inline-item">
-					<a class="footer__link" href="https://twitter.com/tomchristian91">Twitter</a>
+					<a class="footer__icon footer__icon--github" href="https://github.com/tctc91">
+						<i class="icon-github"></i>
+						<span class="sr-only">Github</span>
+					</a>
 				</li>
 				<li class="footer__item footer__item--social list-inline-item">
-					<a class="footer__link" href="https://github.com/tctc91">Github</a>
+					<a class="footer__icon footer__icon--twitter" href="https://twitter.com/tomchristian91">
+						<i class="icon-twitter"></i>
+						<span class="sr-only">Twitter</span>
+					</a>
+				</li>
+				<li class="footer__item footer__item--social list-inline-item">
+					<a class="footer__icon footer__icon--medium" href="https://medium.com/@tomchristian91">
+						<i class="icon-medium"></i>
+						<span class="sr-only">Medium</span>
+					</a>
 				</li>
 			</ul>
 
 			<div class="footer__credits">
-				<a class="footer__link footer__link--credit" href="https://github.com/tctc91/angular2-wordpress-portfolio">
-					&copy; Tom Christian &ndash; Created with ReactJS &amp; Wordpress
-				</a>
+				<span class="footer__link footer__link--credit">
+					&copy; Tom Christian &ndash; Created with React &amp; Wordpress API
+				</span>
 			</div>
 		</div>
 	</footer>
