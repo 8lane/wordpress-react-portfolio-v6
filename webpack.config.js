e6a73627603aea60f4e656a154ce1a0d05dbca88
Tom  Christian <tctc91@gmail.com>
2017-08-09 22:08:25 +0100

    sourcemaps + prod optimisatio

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
@@ -9,6 +10,29 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx'],
   },
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      compress: {
+        warnings: false, // Suppress uglification warnings
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true,
+      },
+      output: {
+        comments: false,
+      },
+      exclude: [/\.min\.js$/gi],
+      sourceMap: true
+    }),
+  ],
   module: {
     loaders: [
       {
@@ -21,6 +45,6 @@ module.exports = {
         exclude: /node_modules/,
         loader: "eslint-loader",
       },
-    ]
-  }
+    ],
+  },
 };
