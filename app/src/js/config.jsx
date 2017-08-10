2b69267cbb6ec8c36a980bf6016793e72664b45e
Tom  Christian <tctc91@gmail.com>
2017-08-10 21:34:00 +0100

    crappy env config added

--- app/src/js/config.jsx
+++ app/src/js/config.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://dev/folio/v6/wordpress/wp-json/wp/v2';
+// const BASE_URL_DEV = 'http://dev/folio/v6/wordpress/wp-json/wp/v2';
+const BASE_URL_PROD = 'http://tomchristian.co.uk/wordpress/wp-json/wp/v2';
+const BASE_URL = BASE_URL_PROD;
 
 const ENDPOINT = {
   PROJECT_TAGS: BASE_URL + '/tags',
