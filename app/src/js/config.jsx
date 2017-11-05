d79ad37aa8ed987c25e923688620a09ddc60d030
Tom  Christian <tctc91@gmail.com>
2017-11-05 13:41:23 +0000

    api

--- app/src/js/config.jsx
+++ app/src/js/config.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://dev/folio/v6/wordpress/wp-json/wp/v2';
-// const API_URL = 'http://tomchristian.co.uk/wp-json/wp/v2';
+// const API_URL = 'http://dev/folio/v6/wordpress/wp-json/wp/v2';
+const API_URL = 'http://tomchristian.co.uk/wp-json/wp/v2';
 const BASE_URL = API_URL;
 
 const ENDPOINT = {
